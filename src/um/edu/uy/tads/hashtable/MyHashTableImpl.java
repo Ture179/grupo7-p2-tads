58a5b0765f818116ccdf115274ddb4da17ce0f8a
JoseUbal <jubal@correo.um.edu.uy>
2025-06-03 10:35:06 -0300

    cambie myhashtableimpl

---
+++ src/um/edu/uy/tads/hashtable/MyHashTableImpl.java
@@ -0,0 +1,30 @@
+package um.edu.uy.tads.hashtable;
+
+import um.edu.uy.exceptions.ElementoYaExistente;
+
+public class MyHashTableImpl<K,V> implements MyHashTable<K,V> {
+
+
+
+
+    @Override
+    public void insertar(K clave, V valor) throws ElementoYaExistente {
+
+
+    }
+
+    @Override
+    public boolean contieneClave(K clave) {
+        return false;
+    }
+
+    @Override
+    public V obtener(K clave) {
+        return null;
+    }
+
+    @Override
+    public void borrar(K clave) {
+
+    }
+}
